b67b83bf1121295ab02d9f72e5a07368243eb261
Abass_Kuku <abasskuku319@gmail.com>
2024-12-20 21:06:32 +0300

    deploying the app

--- scholarship_blog/settings.py
+++ scholarship_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=v&$zf#g=u(im1aaddo$f8tm&jz#dpro#dwj(=oa&1l1w8ij0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lets-educate-kakuma.onrender.com', 'localhost', '127.0.0.1']
 
