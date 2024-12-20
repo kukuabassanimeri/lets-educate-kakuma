1d24f1b4ccc74fb17d27d4f3dfbda1f9bab04930
Abass_Kuku <abasskuku319@gmail.com>
2024-12-20 21:28:22 +0300

    deploying the app

--- scholarship_blog/settings.py
+++ scholarship_blog/settings.py
@@ -83,18 +83,6 @@ DATABASES = {
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'let\'s_educate_kakuma',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
