a9f04e2badf627b54de2e6c77080a0b16fff0e07
Abass_Kuku <abasskuku319@gmail.com>
2024-12-28 10:43:07 +0300

    second commit

--- scholarship_blog/settings.py
+++ scholarship_blog/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "scholarship_post", "static") 
