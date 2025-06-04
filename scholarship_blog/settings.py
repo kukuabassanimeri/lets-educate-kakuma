d6195ec7db13f4225dfec091f4999a1527bffa79
Abass_Kuku <abasskuku319@gmail.com>
2025-06-04 18:23:31 +0300

    Collected static files

--- scholarship_blog/settings.py
+++ scholarship_blog/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-#STATICFILES_STORAGE =  "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE =  "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
